750783e94dc2408a67635552d9c5d965aec9aced
Domas <domas.bitvinskas@me.com>
2018-03-19 19:52:37 +0200

    Messenger in about only

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,26 @@ import Markdown from './Markdown';
 import FullScreenLoading from './FullScreenLoading';
 
 import step from '@bloometry/step';
+const facebookMessengerSource = `
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your customer chat code -->
+  <div class="fb-customerchat"
+    page_id="185075165567392"
+    theme_color="#71ABE2"
+    minimized="true"
+    logged_in_greeting="Labas! Gal turi klausimų?"
+    logged_out_greeting="Labas! Gal turi klausimų?">
+  </div>
+`;
 
 class About extends Component {
   render() {
@@ -43,6 +63,9 @@ class About extends Component {
         </Container>
 
         <Footer {...this.props} />
+        <div dangerouslySetInnerHTML={{
+          __html: facebookMessengerSource,
+        }}/>
       </div>
     );
   }
