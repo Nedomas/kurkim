7303a330aba82906c27b454fad7a0a2b17b8e09d
Domas <domas.bitvinskas@me.com>
2018-03-21 19:37:57 +0200

    Smart post images

--- src/components/MarkdownRenderers/Image.js
+++ src/components/MarkdownRenderers/Image.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
+import imageUrl from '../../theme/imageUrl';
 
 export default class MarkdownRendererImage extends Component {
+  isGraphCmsImage() {
+    return this.props.src.match(/media\.graphcms\.com/);
+  }
+
+  imageHandle() {
+    return _.last(this.props.src.split('/'));
+  }
+
+  src() {
+    if (!this.isGraphCmsImage()) return this.props.src;
+
+    return imageUrl({ handle: this.imageHandle() }, { width: 940 });
+  }
+
   render() {
-    return <img alt='' {...this.props} style={styles.container} />;
+    return <img alt='' {...this.props} src={this.src()} style={styles.container} />;
   }
 }
 
